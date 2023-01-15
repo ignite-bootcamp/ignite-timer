a8960da4b3fbddabbdaf67903a3455ecdc97e79d
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-01-15 04:58:49 -0300

    styling counter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,9 +12,9 @@ export function Header() {
           title="Timer"
           className={({ isActive }) =>
             classnames(
-              'w-12 h-12 flex justify-center items-center text-neutral-200 border-b-emerald-500 border-t-transparent hover:border-b-2 hover:border-t-2',
+              'w-12 h-12 flex justify-center items-center text-neutral-200 border-b-emerald-600 border-t-transparent hover:border-b-2 hover:border-t-2',
               {
-                'text-emerald-500': isActive,
+                'text-emerald-600': isActive,
               },
             )
           }
@@ -26,9 +26,9 @@ export function Header() {
           title="Histórico"
           className={({ isActive }) =>
             classnames(
-              'w-12 h-12 flex justify-center items-center text-neutral-200 border-b-emerald-500 border-t-transparent hover:border-b-2 hover:border-t-2',
+              'w-12 h-12 flex justify-center items-center text-neutral-200 border-b-emerald-600 border-t-transparent hover:border-b-2 hover:border-t-2',
               {
-                'text-emerald-500': isActive,
+                'text-emerald-600': isActive,
               },
             )
           }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,23 +6,36 @@ export default function Home() {
       <form className="flex flex-col items-center gap-14">
         <div className="w-full flex items-center justify-center gap-2 text-neutral-200 text-lg font-bold flex-wrap">
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" type="text" />
+          <input
+            className="flex-1 h-10 font-bold text-lg px-2 bg-transparent border-b-neutral-500 border-b-2 placeholder:text-neutral-500 placeholder:text-center focus:border-b-emerald-500 focus:outline-none"
+            placeholder="Dê um nome para seu projeto"
+            id="task"
+            type="text"
+          />
           <label htmlFor="minutesAmount">durante</label>
-          <input id="minutesAmount" type="number" />
+          <input
+            className="h-10 w-16 font-bold text-lg px-2 bg-transparent border-b-neutral-500 border-b-2 placeholder:text-neutral-500 placeholder:text-center focus:border-b-emerald-500 focus:outline-none"
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+          />
           <span>minutos.</span>
         </div>
 
         <div className="font-mono text-[10rem] leading-[8rem] text-neutral-200 flex gap-4 font-bold">
           <span className="bg-neutral-700/30 py-8 px-4 rounded">0</span>
           <span className="bg-neutral-700/30 py-8 px-4 rounded">0</span>
-          <div className="py-8 text-emerald-500 w-16 overflow-hidden flex justify-center">
+          <div className="py-8 text-emerald-600 w-16 overflow-hidden flex justify-center">
             :
           </div>
           <span className="bg-neutral-700/30 py-8 px-4 rounded">0</span>
           <span className="bg-neutral-700/30 py-8 px-4 rounded">0</span>
         </div>
 
-        <button type="submit">
+        <button
+          type="submit"
+          className="bg-emerald-600 text-neutral-200 flex items-center justify-center rounded-lg w-full p-4 gap-2 cursor-pointer font-bold enabled:hover:bg-emerald-700 transition disabled:bg-opacity-70 disabled:cursor-not-allowed"
+        >
           <Play size={24} />
           Começar
         </button>
