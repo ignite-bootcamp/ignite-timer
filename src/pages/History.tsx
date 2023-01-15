0ab072ba558aba4087fcef7ef317da4f364c8b3a
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-01-15 06:58:51 -0300

    history page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,3 +1,44 @@
 export default function History() {
-  return <h1>history</h1>
+  return (
+    <main className="flex-1 p-14 flex flex-col">
+      <h1 className="text-2xl text-neutral-200">Meu histórico</h1>
+
+      <div className="flex-1 overflow-auto mt-8">
+        <table className="w-full border-collapse min-w-[600px]">
+          <thead>
+            <tr>
+              <th className="bg-neutral-700/50 p-4 pl-6 text-left text-neutral-200 text-sm rounded-tl-lg">
+                Tarefa
+              </th>
+              <th className="bg-neutral-700/50 p-4 text-left text-neutral-200 text-sm">
+                Duração
+              </th>
+              <th className="bg-neutral-700/50 p-4 text-left text-neutral-200 text-sm">
+                Inicio
+              </th>
+              <th className="bg-neutral-700/50 p-4 pr-6 text-left text-neutral-200 text-sm rounded-tr-lg">
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="w-1/2 bg-neutral-700/30 border-t-4 border-t-neutral-800/80 text-sm p-4">
+                Tarefa
+              </td>
+              <td className="bg-neutral-700/30 border-t-4 border-t-neutral-800/80 text-sm p-4">
+                25 minutos
+              </td>
+              <td className="bg-neutral-700/30 border-t-4 border-t-neutral-800/80 text-sm p-4">
+                Há cerca de 2 horas
+              </td>
+              <td className="bg-neutral-700/30 border-t-4 border-t-neutral-800/80 text-sm p-4">
+                Concluído
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
 }
